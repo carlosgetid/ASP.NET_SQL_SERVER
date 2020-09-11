ddc3deb2885609da23f5e1bf2484aca290327d15
paulable <carlospgp13@gmail.com>
2020-09-11 01:04:11 -0500

    Controller

---
+++ Lab04/Controllers/ConsultasController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using System.Configuration;
+using System.Data.SqlClient;
+using Lab04.Models;
+using System.Data;
+
+namespace Lab04.Controllers
+{
+    public class ConsultasController : Controller
+    {
+        SqlConnection cn = new SqlConnection(
+            ConfigurationManager.ConnectionStrings["CNX"].ConnectionString);
+
+        List<Cliente> buscarClientes(String nombre)
+        {
+            List<Cliente> resultado = new List<Cliente>();
+            SqlCommand cmd = new SqlCommand("sp_buscar_clientes", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@Nombre", nombre);
+            cn.Open();
+
+            SqlDataReader dr = cmd.ExecuteReader();
+            while (dr.Read())
+            {
+                Cliente c = new Cliente();
+                c.idCliente = Convert.ToInt32(dr["idcliente"].ToString());
+                c.nombre = dr["nombre"].ToString();
+                c.direccion = dr["direccion"].ToString();
+                c.idPais = Convert.ToInt32(dr["idpais"].ToString());
+                c.telefono = dr["telefono"].ToString();
+                resultado.Add(c);
+            }
+            dr.Close();
+            cn.Close();
+
+            return resultado;
+        }
+        
+        public ActionResult busquedaClientes(String nombre)
+        {
+            List<Cliente> lista = buscarClientes(nombre);
+
+            return View(lista);
+        }
+
+        // GET: Consultas
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lab04/Models/Cliente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab04.Models
+{
+    public class Cliente
+    {
+        public int idCliente { get; set; }
+        public String nombre { get; set; }
+        public String direccion { get; set; }
+        public String telefono { get; set; }
+        public int idPais { get; set; }
+        public string nombrePais { get; set; }
+    }
+}
